95c4588163e449e45ed4047adae0632bca0faaf9
Julián Hernández Muñoz <jchernan@alum.mit.edu>
2013-02-10 11:49:24 -0500

    Add script to generate video html

--- main.js
+++ main.js
@@ -19,9 +19,47 @@ var Resume = function () {
       content.push('<dt>' + title + '</dt>');
       content.push('<dd>' + institute + ' ' + date + '</dd>');
     });
-    $.each(resume, function (key, val) {
-      var html = '<dl>' + val.join('') + '</dl>';
-      $(html).appendTo('#' + key);
+    $.each(resume, function (category, content) {
+      var html = '<dl>' + content.join('') + '</dl>';
+      $(html).appendTo('#' + category);
+    });
+  });
+
+}();
+
+var Video = function () {
+
+  var jsonFile = 'video.json';
+  var videos = [];
+
+  var header = '<div class="row">'
+    + '<div class="offset1">'
+    + '<iframe width="420" height="315"';
+  var footer = 'frameborder="0" allowfullscreen>'
+    + '</iframe>';
+
+  $.getJSON(jsonFile, function (data) {
+    $.each(data.video, function (i, video) {
+      var content = [];
+      content.push(header);
+      content.push('src="' + video.link + '"');
+      content.push(footer);
+      content.push('<dl>');
+      content.push('<dt>' + video.title + '</dt>');
+      $.each(video.festival, function (j, festival) {
+        var date = '<span class="date">(' + festival.date + ')</span>';
+        content.push('<dd>' + festival.name + ' ' + date + '</dd>');
+        $.each(festival.awards, function (k, award) {
+          content.push('<dd class="award">' + award + '</dd>');
+        });
+      });
+      content.push('</dl></div></div>');
+      videos.push(content);
+    });
+
+    $.each(videos, function (i, content) {
+      var html = content.join('');
+      $(html).appendTo('#Video');
     });
   });
 
